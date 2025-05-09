d633d6c57604cad4a3467342f2beda47ef8efd2d
Vinicius <vinniciussramallho98@gmail.com>
2025-05-09 11:21:24 -0300

    route to page login

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import InitialLogin from './pages/InitialLogin';
-import PreHeader from './components/PreHeader';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import InitialLogin from "./pages/InitialLogin";
+import UnderConstruction from "./components/UnderConstruction";
 
 
 function App() {
   return (
     <Router>
-
-
       <Routes>
         <Route path="/" element={<InitialLogin />} />
+        <Route path="/construction" element={<UnderConstruction />} />
       </Routes>
-
     </Router>
   );
 }
