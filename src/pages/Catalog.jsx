c0ef43635ff36330ffc591898af560fd3f389c0d
Vinicius <vinniciussramallho98@gmail.com>
2025-05-10 12:59:36 -0300

    construction page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,16 +1,12 @@
 import UnderConstruction from "../components/UnderConstruction";
 
-const Catalog = () => {
-  const phone = "5585985009462"; // Código do país + DDD + número
-  const message = encodeURIComponent(
-    "Olá! Estou entrando em contato pelo app."
-  );
-  const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
 
+const Catalog = () => {
   return (
-    <a href={whatsappUrl} target="_blank" rel="noopener noreferrer">
-      Enviar mensagem no WhatsApp
-    </a>
+    <div>
+      
+      <UnderConstruction/>
+    </div>
   );
 };
 
