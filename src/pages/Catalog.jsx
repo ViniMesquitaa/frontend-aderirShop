579fe6f106cba6813ca6f1e2e87a8b99bb1b5a6d
Vinicius <vinniciussramallho98@gmail.com>
2025-05-12 10:13:54 -0300

    Catalog carrossel

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,78 @@
-import UnderConstruction from "../components/UnderConstruction";
+import SubHeader from "../components/SubHeader";
+import Carrossel from "../components/Carrossel";
 
+const sabaoProducts = [
+  {
+    id: 1,
+    name: "Sabão em Pó Omo",
+    description: "Sabão em pó para roupas brancas e coloridas.",
+    price: 19.99,
+    stock: 50,
+  },
+  {
+    id: 2,
+    name: "Sabão Líquido Ariel",
+    description: "Sabão líquido concentrado para roupas delicadas.",
+    price: 24.99,
+    stock: 30,
+  },{
+      id: 1,
+    name: "Sabão em Pó Omo",
+    description: "Sabão em pó para roupas brancas e coloridas.",
+    price: 19.99,
+    stock: 50,
+  }, {
+    id: 2,
+    name: "Sabão Líquido Ariel",
+    description: "Sabão líquido concentrado para roupas delicadas.",
+    price: 24.99,
+    stock: 30,
+  },  {
+    id: 1,
+    name: "Sabão em Pó Omo",
+    description: "Sabão em pó para roupas brancas e coloridas.",
+    price: 19.99,
+    stock: 50,
+  },
+  {
+    id: 2,
+    name: "Sabão Líquido Ariel",
+    description: "Sabão líquido concentrado para roupas delicadas.",
+    price: 24.99,
+    stock: 30,
+  },{
+      id: 1,
+    name: "Sabão em Pó Omo",
+    description: "Sabão em pó para roupas brancas e coloridas.",
+    price: 19.99,
+    stock: 50,
+  }, {
+    id: 2,
+    name: "Sabão Líquido Ariel",
+    description: "Sabão líquido concentrado para roupas delicadas.",
+    price: 24.99,
+    stock: 30,
+  },
+]
 
 const Catalog = () => {
   return (
-    <div>
-      
-      <UnderConstruction/>
+    <div className=" mx-auto">
+      <div className="items-center bg-white shadow-md rounded-lg mb-6">
+        <SubHeader />
+      </div>
+   
+      <div className="mx-auto max-w-7xl  overflow-x-hidden ">
+        <h2 className="text-2xl font-bold text-gray-800">Sabão</h2>
+        <Carrossel products={sabaoProducts} />
+      </div>
+
+      <div className="mx-auto max-w-7xl  overflow-x-hidden">
+        <Carrossel />
+      </div>
+      <div className="mx-auto max-w-7xl  overflow-x-hidden">
+        <Carrossel />
+      </div>
     </div>
   );
 };
