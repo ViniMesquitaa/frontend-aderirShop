fe9c70c9a01b6e7bcfa76ce238b7ccc63ff32e38
Vinicius <vinniciussramallho98@gmail.com>
2025-05-12 10:12:17 -0300

    UI to order products

---
+++ src/components/OrderByUI.jsx
@@ -0,0 +1,26 @@
+import { FiFilter } from "react-icons/fi";
+
+const OrderByUI = () => {
+  return (
+    <div className="flex items-center justify-center w-full sm:w-auto">
+      <div className="relative w-full sm:w-60">
+        <FiFilter className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
+
+        <select
+          id="orderBy"
+          name="orderBy"
+          className="w-full rounded-2xl border border-gray-300 bg-white py-2 pl-10 pr-3 shadow-sm focus:border-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all text-[15px] font-medium"
+        >
+          <option value="filtrar_produtos">Todos</option>
+          <option value="sabao">Sabão</option>
+          <option value="detergente">Detergente</option>
+          <option value="amaciante">Amaciante</option>
+          <option value="desinfetante">Desinfetante</option>
+          <option value="alvejante">Alvejante</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default OrderByUI;
